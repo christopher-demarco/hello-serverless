11842a3b824d807cc1a84a663032614ea0e0fd87
Christopher DeMarco <cdemarco@gmail.com>
2021-05-02 00:34:49 -0400

    Make a Lambda

---
+++ terraform/terraform.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-west-2"
+  profile = "nuvalence"
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "nuvalence-lambda-role"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_lambda_function" "hello_lambda" {
+  filename = "../bin/hello.zip"
+  function_name = "hello-nuvalence"
+  handler = "main"
+  role = aws_iam_role.iam_for_lambda.arn
+  source_code_hash = filebase64sha256("../bin/hello.zip")
+  runtime = "python3.6"
+}
